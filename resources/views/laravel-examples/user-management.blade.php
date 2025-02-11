5edb152250c7c76b150287df0c25806e6366ccaf
Wilmar Peña <wpenarudas@gmail.com>
2025-02-11 16:04:11 -0500

    Updating fonts

--- resources/views/laravel-examples/user-management.blade.php
+++ resources/views/laravel-examples/user-management.blade.php
@@ -7,7 +7,7 @@
         <span class="text-white">
             <strong>Add, Edit, Delete features are not functional!</strong> This is a
             <strong>PRO</strong> feature! Click <strong>
-            <a href="https://www.creative-tim.com/live/soft-ui-dashboard-pro-laravel" target="_blank" class="text-white">here</a></strong>
+            <a href="#" target="_blank" class="text-white">here</a></strong>
             to see the PRO product!
         </span>
     </div>
--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -5,27 +5,26 @@
                 <div class="copyright text-center text-sm text-muted text-lg-start">
                     © <script>
                         document.write(new Date().getFullYear())
-                    </script>, made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>&amp; <a style="color: #252f40;" href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>
-                    for a better web.
+                    </script>, made <i class="fa fa-heart"></i> by
+                    <a href="#" class="font-weight-bold" target="_blank">Wilmar J. Peña Rudas</a>
                 </div>
             </div>
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative Tim</a>
+                        <a href="#" class="nav-link text-muted">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.updivision.com" class="nav-link text-muted" target="_blank">UPDIVISION</a>
+                        <a href="#" class="nav-link text-muted">Jobs</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+                        <a href="#" class="nav-link text-muted">About Us</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://creative-tim.com/blog" class="nav-link text-muted" target="_blank">Blog</a>
+                        <a href="#" class="nav-link text-muted">Blog</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                        <a href="#" class="nav-link pe-0 text-muted">License</a>
                     </li>
                 </ul>
             </div>
